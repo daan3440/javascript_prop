0d50004eb4d6ccd74123b05321fb22d2dae87b6c
daan3440 <daan3440@su.se>
2018-11-27 09:00:07 +0100

    Testline

--- index.js
+++ index.js
@@ -54,3 +54,25 @@ myObject.checkParentTree = function (wantedParent) {
     } 
     return false;
 };
+
+
+//Testpart 1
+var obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+var obj1 = myObject.create([obj0]);
+var obj2 = myObject.create([]);
+obj2.func = function(arg) { return "func2: " + arg; };
+var obj3 = myObject.create([obj1, obj2]);
+var result = obj3.call("func", ["hello"])  ;
+console.log("should print ’func0: hello’ ->", result);
+
+result = obj0.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
+
+//Tast Part 2
+//var obj0 = myObject.create(null);
+//var obj1 = myObject.create([obj0]);
+var obj4 = myObject.create([obj1]);
+
+obj0.addPrototype(obj1);
+obj4.addPrototype(obj3);
