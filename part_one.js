5d613fb597c5009ee17eb3bebf77651f65a1a13c
daan3440 <daan3440@su.se>
2018-11-29 14:54:14 +0100

    selfcheck updated

---
+++ part_one.js
@@ -0,0 +1,101 @@
+var myObject = {
+};
+
+myObject.create = function (prototypeList, name) {
+    o = {};
+    o.name = name;
+    o.prototypeList = prototypeList;
+    o.__proto__ = this;
+    return o;
+};
+
+myObject.call = function (functionName, args) {
+    if (!(typeof this[functionName] === 'function')) {
+        if (this.prototypeList !== null) {
+            for (let i = 0; i < this.prototypeList.length; i++) {
+                let result = this.prototypeList[i].call(functionName, args);
+                if(result != undefined){
+                    return result;
+                }   
+            }
+        }
+    } else {
+        return this[functionName].apply(null, args);
+    }
+};
+
+myObject.addPrototype = function (wantedParent) {
+    if(wantedParent.checkInheritanceTreeForChild(this)){
+        console.log("Error: Circular Inheritance not allowed!");
+    } else {
+        if(this.prototypeList === null){
+            this.prototypeList = [];
+        }
+        if(!this.prototypeList.includes(wantedParent)){
+            this.prototypeList.push(wantedParent);
+        }
+    }
+};
+
+myObject.checkInheritanceTreeForChild = function (potentialChild) {
+    if (this.prototypeList !== null) {
+        for (let i = 0; i < this.prototypeList.length; i++) {
+            if (potentialChild === this.prototypeList[i]) {
+                return true;
+            } else {
+                parentFound = this.prototypeList[i].checkInheritanceTreeForChild(potentialChild);
+                if (parentFound) {
+                    return true;
+                }
+            }
+        }
+    } 
+    return false;
+};
+
+//check 1
+var obj0 = myObject.create(null, "obj0");
+obj0.func = function(arg, args) { return "func0: " + arg; };
+var obj1 = myObject.create([obj0], "obj1");
+var obj2 = myObject.create([], "obj2");
+obj2.func = function(arg) { return "func2: " + arg; };
+var obj3 = myObject.create([obj1, obj2], "obj3");
+var result = obj3.call("func", ["hello"]) ;
+console.log("should print ’func0: hello’ ->", result);
+
+//check 2
+obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+obj1 = myObject.create([obj0]);
+obj2 = myObject.create([]);
+obj3 = myObject.create([obj2, obj1]);
+result = obj3.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
+//Check 3
+obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+result = obj0.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
+
+//check circular prevention
+var obj0 = myObject.create(null);
+var obj1 = myObject.create([obj0]);
+obj0.addPrototype(obj1);
+
+//egna tester
+// var obj0 = myObject.create(null, "obj0");
+// var obj1 = myObject.create([obj0], "obj1");
+// var obj3 = myObject.create([obj1], "obj3");
+// var obj4 = myObject.create([obj1, obj3], "obj4");
+// var obj5 = myObject.create([obj3],"obj5");
+// var obj6 = myObject.create([obj0, obj5],"obj6");
+// var obj2 = myObject.create([obj0, obj5],"obj2b");
+
+// obj3.addPrototype(obj2);
+// obj3.addPrototype(obj1);
+// obj5.addPrototype(obj4);
+
+// console.log(obj2);
+// console.log(obj5);
+// console.log(obj3);
+// console.log(obj6);
---
+++ part_two.js
@@ -0,0 +1,122 @@
+function createClass(className, superClassList) {
+    myClass = {
+        name: className,
+        superList: superClassList,
+        new: function () {
+            o = {};
+            o.class = this;
+            o.call = function (funcName, parameters) {
+                if (!(typeof this[funcName] === 'function')) {
+                    return this.class.lookUp(funcName, parameters);
+                } else {
+                    return this[funcName].apply(null, parameters);
+                }
+
+            };
+            return o;
+        },
+        //TODO kanske kolla om vi kan förändra scope till Class.lookup()
+        lookUp: function (funcName, parameters) {
+            if (!(typeof this[funcName] === 'function')) {
+                if (this.superList !== null) {
+                    for (var i = 0; i < this.superList.length; i++) {
+                        var result = this.superList[i].lookUp(funcName, parameters);
+                        if (result != undefined) {
+                            return result;
+                        }
+
+                    }
+                }
+            } else {
+                return this[funcName].apply(null, parameters);
+            }
+        },
+        addSuperClass: function (makeSuperClass) {
+            if (makeSuperClass === this || makeSuperClass.checkNoneValidSuperClass(this)) {
+                console.log("Error: invalid superClass, circular inheritance detected.");
+            } else {
+                if (this.superList === null) {
+                    this.superList = [];
+                }
+                if (!this.superList.includes(makeSuperClass)){ 
+                this.superList.push(makeSuperClass);
+            }
+        }
+
+    },
+        checkNoneValidSuperClass: function (possibleChildClass) {
+            // superClassFound = false;
+            if (this.superList !== null) {
+                for (let i = 0; i < this.superList.length; i++) {
+                    if (this.superList[i] === possibleChildClass) {
+                        return true;
+                    } else {
+                        // superClassFound = this.superList[i].checkNoneValidSuperClass(possibleSuperclass);
+                        if (this.superList[i].checkNoneValidSuperClass(possibleChildClass)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        },
+
+};
+return myClass;
+}
+
+//check 1
+var class0 = createClass("Class0", null);
+class0.func = function(arg) { return "func0: " + arg; };
+var class1 = createClass("Class1", [class0]);
+var class2 = createClass("Class2", []);
+class2.func = function(arg) { return "func2: " + arg; };
+var class3 = createClass("Class3", [class1, class2]);
+var obj3 = class3.new();
+var result = obj3.call("func", ["hello"]);
+console.log(result);
+//Check 2
+class0 = createClass("Class0", null);
+class0.func = function(arg) { return "func0: " + arg; };
+class1 = createClass("Class1", [class0]);
+class2 = createClass("Class2", []);
+class3 = createClass("Class3", [class2, class1]);
+obj3 = class3.new();
+result = obj3.call("func", ["hello"]);
+console.log(result);
+//check 3
+class0 = createClass("Class0", null);
+class0.func = function(arg) { return "func0: " + arg; };
+var obj0 = class0.new();
+result = obj0.call("func", ["hello"]);
+console.log(result);
+
+//Check Circualar Prevention
+var class0 = createClass("Class 0", null);
+var class1 = createClass("Class 1", [class0]);
+class0.addSuperClass(class1);
+class0.addSuperClass(class0);
+
+
+
+//Egna tester
+// var class0 = createClass("Class0", null);
+// class0.func = function (arg) { return "func0: " + arg; };
+// var class1 = createClass("Class1", [class0]);
+// var class2 = createClass("Class2", []);
+// class2.func = function (arg) { return "func2: " + arg; };
+// var class3 = createClass("Class3", [class1, class2]);
+// var class4 = createClass("Class3", null);
+// var obj3 = class3.new();
+// var result = obj3.call("func", ["hello"]);
+
+// console.log(result);
+// console.log("class4 + class1");
+// class4.addSuperClass(class1);
+// console.log("class1 + class0");
+// class1.addSuperClass(class0);
+
+// console.log(class1 );
+// console.log("class0 + class1: fail" );
+
+// class0.addSuperClass(class1);
