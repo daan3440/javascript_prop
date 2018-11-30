da2d5e66145faf17fcdbad5a9fb50bc1e7f99f2f
erla5605 <43401426+erla5605@users.noreply.github.com>
2018-11-30 12:47:16 +0100

    Updated with error message on call

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var myObject = {
 };
 
@@ -6,13 +5,20 @@ myObject.create = function (prototypeList, name) {
     o = {};
     o.name = name;
     o.prototypeList = prototypeList;
-    //o.call = this.call;
-    //o.create = this.create;
     o.__proto__ = this;
     return o;
 };
 
-myObject.call = function (functionName, args) {
+myObject.call = function(functionName, args) {
+	let result = this.lookUp(functionName, args);
+	if(result === undefined){
+		console.log("Error, no function with the name " + functionName;
+	} else {
+		return result[0];
+	}		
+}
+
+myObject.lookUp = function (functionName, args) {
     if (!(typeof this[functionName] === 'function')) {
         if (this.prototypeList !== null) {
             for (let i = 0; i < this.prototypeList.length; i++) {
@@ -23,12 +29,12 @@ myObject.call = function (functionName, args) {
             }
         }
     } else {
-        return this[functionName].apply(null, args);
+        return {this[functionName].apply(null, args)};
     }
 };
 
 myObject.addPrototype = function (wantedParent) {
-    if(wantedParent.checkInheritanceTreeForChild(this)){
+    if(wantedParent === this || wantedParent.checkInheritanceTreeForChild(this)){
         console.log("Error: Circular Inheritance not allowed!");
     } else {
         if(this.prototypeList === null){
@@ -56,6 +62,7 @@ myObject.checkInheritanceTreeForChild = function (potentialChild) {
     return false;
 };
 
+//check 1
 var obj0 = myObject.create(null, "obj0");
 obj0.func = function(arg, args) { return "func0: " + arg; };
 var obj1 = myObject.create([obj0], "obj1");
@@ -65,20 +72,39 @@ var obj3 = myObject.create([obj1, obj2], "obj3");
 var result = obj3.call("func", ["hello"]) ;
 console.log("should print ’func0: hello’ ->", result);
 
+//check 2
+obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+obj1 = myObject.create([obj0]);
+obj2 = myObject.create([]);
+obj3 = myObject.create([obj2, obj1]);
+result = obj3.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
+//Check 3
+obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+result = obj0.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
 
+//check circular prevention
+var obj0 = myObject.create(null);
+var obj1 = myObject.create([obj0]);
+obj0.addPrototype(obj1);
+
+//egna tester
 // var obj0 = myObject.create(null, "obj0");
 // var obj1 = myObject.create([obj0], "obj1");
 // var obj3 = myObject.create([obj1], "obj3");
 // var obj4 = myObject.create([obj1, obj3], "obj4");
 // var obj5 = myObject.create([obj3],"obj5");
-// var obj2 = myObject.create([obj0, obj5],"obj2");
+// var obj6 = myObject.create([obj0, obj5],"obj6");
+// var obj2 = myObject.create([obj0, obj5],"obj2b");
 
 // obj3.addPrototype(obj2);
-
 // obj3.addPrototype(obj1);
-
 // obj5.addPrototype(obj4);
 
 // console.log(obj2);
 // console.log(obj5);
 // console.log(obj3);
+// console.log(obj6);
